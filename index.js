f007934eb8c0a99e15586fd3e71e93ecde96da5c
smasyutin <serg.masyutin@gmail.com>
2018-08-06 16:11:41 -0700

    As parallel transactions currently fail, this is the update for the sequential `for` loop, so that transactions are executed sequentially.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApiMarketClient } = require('@apimarket/apimarket');
 const config = require("./apimarket_config.json");
+const delay = require('delay');
 
 const bucket = "http://storage.googleapis.com/apimarket-contest-2018-07-1-coffee";
 
@@ -26,12 +27,18 @@ const run = async (bucket, files) => {
         //specify the api to call using it's unique name registered on the ORE blockchain
         const apiName = "cloud.hadron.contest-2018-07";
 
-        files.forEach(async (file) => {
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+
             const params = {"imageurl":`${bucket}/${file}.jpg`};
             //call api - passing in the data it needs
             const response = await apimarketClient.fetch(apiName, params);
             console.log(`Execution for ${file} \n${JSON.stringify(response, null, 2)}\n`);
-        });
+
+            // delay for 2 second to prevent transaction override exception:
+            // previous transaction must complete before we start a new one
+            await delay(2000);
+        }
     }
     catch(error) {
         console.error(error);
